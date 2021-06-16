121fd19d9c911d1542dfe79aa7e2ab9a9c79a70d
Peerakarn Thongsata <60547443+T0peerakarn@users.noreply.github.com>
2021-06-16 15:19:05 +0700

    Update 1052.cpp

--- beta_programming/1052.cpp
+++ beta_programming/1052.cpp
@@ -16,7 +16,7 @@ priority_queue<range> heap;
 
 int push_left(int i, int n)
 {
-	int pos = p[i] - h[i];
+	int j = i, pos = p[i] - h[i];
 	while(i > 1)
 	{
 		if(pos < p[i-1])
@@ -26,12 +26,13 @@ int push_left(int i, int n)
 		}
 		else break;
 	}
+	heap.push({j, j-i+1, 'L'});
 	return i;
 }
 
 int push_right(int i, int n)
 {
-	int pos = p[i] + h[i];
+	int j = i, pos = p[i] + h[i];
 	while(i < n)
 	{
 		if(p[i+1] < pos)
@@ -41,6 +42,7 @@ int push_right(int i, int n)
 		}
 		else break;
 	}
+	heap.push({j, i-j+1, 'R'});
 	return i;
 }
 
@@ -49,18 +51,8 @@ int main()
 	int n;
 	scanf(" %d",&n);
 	for(int i=1 ; i<=n ; i++) scanf(" %d %d",&p[i],&h[i]);
-	for(int i=n ; i>=1 ; i--)
-	{
-		int j = push_left(i, n);
-		heap.push({i, i-j+1, 'L'});
-		i = j;
-	}
-	for(int i=1 ; i<=n ; i++)
-	{
-		int j = push_right(i, n);
-		heap.push({i, j-i+1, 'R'});
-		i = j;
-	}
+	for(int i=n ; i>=1 ; i = push_left(i, n)-1);
+	for(int i=1 ; i<=n ; i = push_right(i, n)+1);
 	printf("%d %c\n",heap.top().no,heap.top().c);
 	return 0;
 }
