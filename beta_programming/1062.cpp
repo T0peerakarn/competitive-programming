d680162aed033555844886b990f6ac518f6307c8
Peerakarn Thongsata <60547443+T0peerakarn@users.noreply.github.com>
2021-06-16 18:30:38 +0700

    Update 1062.cpp

--- beta_programming/1062.cpp
+++ beta_programming/1062.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char a[130], b[130], c[130];
+char a[130], b[130];
 
 int main()
 {
 	int n, m;
 	scanf(" %d %d",&n,&m);
-	scanf(" %s %s %s",a,b,c);
+	scanf(" %s %s",a,b);
 	for(int i=0 ; i<m ; i++)
 	{
+		char c;
+		scanf(" %c",&c);
 		for(int j=0 ; j<n ; j++)
 		{
-			vector<char> v = {a[j], b[j], c[i]};
+			vector<char> v = {a[j], b[j], c};
 			sort(v.begin(), v.end());
-			c[i] = v[1];
+			c = v[1];
 		}
-		printf("%c",c[i]);
+		printf("%c",c);
 	}
 	return 0;
 }
