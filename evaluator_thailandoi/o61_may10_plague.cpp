72a57483a452081970addf5349b8842eba9677bb
Peerakarn Thongsata <60547443+T0peerakarn@users.noreply.github.com>
2021-07-24 02:48:58 +0700

    Update o61_may10_plague.cpp

--- evaluator_thailandoi/o61_may10_plague.cpp
+++ evaluator_thailandoi/o61_may10_plague.cpp
@@ -1,112 +1,99 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#pragma GCC optimize("Ofast")
 using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
-
-template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#pragma GCC optimize("Ofast")
 
 const int N = 1e5+5;
 
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 int cnt;
-int sub[N], lv[N], pa[20][N], jmp[20][N], dis[20][N];
-vector<int> g[N], gt[N];
-bool visit[N];
+int sub[N], dis[N][20], pa[N], lv[N];
+bool processed[N];
+vector<int> g[N];
 ordered_set<pair<int, int>> t1[N], t2[N];
 
 int get_sub(int u, int p)
 {
-	sub[u] = 1;
-	for(auto v : g[u])
-	{
-		if(v == p || visit[v]) continue;
-		sub[u] += get_sub(v, u);
-	}
-	return sub[u];
+    sub[u] = 1;
+    for(int v : g[u]) if(v != p && !processed[v]) sub[u] += get_sub(v, u);
+    return sub[u];
 }
 
 int get_centroid(int u, int p, int n)
 {
-	bool ch = (n-sub[u] <= n/2);
-	int uu = 0;
-	for(auto v : g[u])
-	{
-		if(v == p || visit[v]) continue;
-		if(sub[v] > n/2)
-		{
-			uu = v;
-			ch = false;
-		}
-	}
-	return (ch) ? u : get_centroid(uu, u, n);
+    for(int v : g[u]) if(v != p && !processed[v] && sub[v] > n/2) return get_centroid(v, u, n);
+    return u;
 }
 
-void gen_distance(int u, int p, int l)
+void get_distance(int u, int p, int l)
 {
-	for(auto v : g[u])
-	{
-		if(v == p || visit[v]) continue;
-		dis[l][v] = dis[l][u]+1;
-		gen_distance(v, u, l);
-	}
+    for(int v : g[u]) if(v != p && !processed[v])
+    {
+        dis[v][l] = dis[u][l] + 1;
+        get_distance(v, u, l);
+    }
 }
 
 int build_centroid(int u, int p)
 {
-	int centroid = get_centroid(u, p, get_sub(u, p));
-	visit[centroid] = true;
-	lv[centroid] = lv[p]+1;
-	gen_distance(centroid, 0, lv[centroid]);
-	for(auto v : g[centroid])
-	{
-		if(visit[v]) continue ;
-		pa[1][build_centroid(v, centroid)] = centroid;
-	}
-	return centroid;
+    u = get_centroid(u, 0, get_sub(u, 0));
+    processed[u] = true;
+    get_distance(u, 0, lv[u] = lv[p]+1);
+    for(int v : g[u]) if(!processed[v]) pa[build_centroid(v, u)] = u;
+    return u;
+}
+
+void update(int u, int k)
+{
+    t1[u].insert({k, ++cnt});
+    for(int now=pa[u], prv=u ; now ; now=pa[now], prv=pa[prv]) if(k >= dis[u][lv[now]])
+    {
+        t1[now].insert({k-dis[u][lv[now]], ++cnt});
+        t2[prv].insert({k-dis[u][lv[now]], ++cnt});
+    }
+}
+
+int query(int u)
+{
+    int ret = t1[u].size();
+    for(int now=pa[u], prv=u ; now ; now=pa[now], prv=pa[prv])
+    {
+        ret += t1[now].size() - t1[now].order_of_key({dis[u][lv[now]], 0});
+        ret -= t2[prv].size() - t2[prv].order_of_key({dis[u][lv[now]], 0});
+    }
+    return ret;
 }
 
 int main()
 {
-	int n, q;
-	scanf(" %d %d",&n,&q);
-	for(int i=1 ; i<n ; i++)
-	{
-		int u, v;
-		scanf(" %d %d",&u,&v);
-		g[u].push_back(v), g[v].push_back(u);
-	}
-	build_centroid(1, 0);
-	for(int i=1 ; i<=n ; i++)
-	{
-		pa[0][i] = i;
-		for(int j=2 ; j<20 ; j++) pa[j][i] = pa[1][pa[j-1][i]];
-	}
-	while(q--)
-	{
-		int op, a, b;
-		scanf(" %d",&op);
-		if(op == 1)
-		{
-			scanf(" %d %d",&a,&b);
-			t1[a].insert(make_pair(b, ++cnt));
-			for(int i=1 ; pa[i][a] != 0 ; i++)
-			{
-				if(b < dis[lv[pa[i][a]]][a]) continue ;
-				t1[pa[i][a]].insert(make_pair(b-dis[lv[pa[i][a]]][a], ++cnt));
-				t2[pa[i-1][a]].insert(make_pair(b-dis[lv[pa[i][a]]][a], ++cnt));
-			}
-		}
-		else
-		{
-			scanf(" %d",&a);
-			int ans = t1[a].size() - t1[a].order_of_key(make_pair(0, 0));
-			for(int i=1 ; pa[i][a] != 0 ; i++)
-			{
-				ans += t1[pa[i][a]].size() - t1[pa[i][a]].order_of_key(make_pair(dis[lv[pa[i][a]]][a], 0));
-				ans -= t2[pa[i-1][a]].size() - t2[pa[i-1][a]].order_of_key(make_pair(dis[lv[pa[i][a]]][a], 0));
-			}
-			printf("%d\n",ans);
-		}
-	}
-	return 0;
+    int n, q;
+    scanf(" %d %d",&n,&q);
+    for(int i=1 ; i<n ; i++)
+    {
+        int u, v;
+        scanf(" %d %d",&u,&v);
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    build_centroid(1, 0);
+    while(q--)
+    {
+        int op;
+        scanf(" %d",&op);
+        if(op == 1)
+        {
+            int u, k;
+            scanf(" %d %d",&u,&k);
+            update(u, k);
+        }
+        else
+        {
+            int u;
+            scanf(" %d",&u);
+            printf("%d\n",query(u));
+        }
+    }
+    return 0;
 }
