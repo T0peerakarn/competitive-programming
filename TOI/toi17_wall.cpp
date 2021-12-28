c496ebed5c7fcebb82d8fb00bfdf7bc36a6debff
Peerakarn Thongsata <60547443+T0peerakarn@users.noreply.github.com>
2021-12-28 21:43:38 +0700

    Update toi17_wall.cpp

--- TOI/toi17_wall.cpp
+++ TOI/toi17_wall.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 const int N = 1e3+5;
 
-int comp[N][N], di[4] = {1, -1, 0, 0}, dj[4] = {0, 0, 1, -1}, cnt[N*N];
+int di[4] = {1, -1, 0, 0}, dj[4] = {0, 0, 1, -1};
 char t[N][N];
-bool mark[N*N];
+bool water[N][N];
 
 int main()
 {
@@ -14,33 +14,28 @@ int main()
     scanf(" %d %d",&n,&m);
     for(int i=1 ; i<=n ; i++) for(int j=1 ; j<=m ; j++) scanf(" %c",&t[i][j]);
 
-    for(int i=1 ; i<=n ; i++) for(int j=1 ; j<=m ; j++) if(t[i][j] == '.')
+    water[1][1] = true;
+    queue<pair<int, int>> bfs;
+    bfs.push({1, 1});
+    while(!bfs.empty())
     {
-        comp[i][j] = ++cnt_comp;
-        t[i][j] = ' ';
-        queue<pair<int, int>> bfs;
-        bfs.push({i, j});
-        while(!bfs.empty())
+        int ni = bfs.front().first;
+        int nj = bfs.front().second;
+        bfs.pop();
+        for(int k=0 ; k<4 ; k++)
         {
-            int ni = bfs.front().first;
-            int nj = bfs.front().second;
-            bfs.pop();
-            for(int k=0 ; k<4 ; k++)
-            {
-                int ii = ni+di[k], jj = nj+dj[k];
-                if(ii < 1 || jj < 1 || ii > n || jj > m || t[ii][jj] != '.') continue;
-                comp[ii][jj] = cnt_comp;
-                t[ii][jj] = ' ';
-                bfs.push({ii, jj});
-            }
+            int ii = ni+di[k], jj = nj+dj[k];
+            if(ii < 1 || jj < 1 || ii > n || jj > m || t[ii][jj] != '.' || water[ii][jj]) continue;
+            water[ii][jj] = true;
+            bfs.push({ii, jj});
         }
     }
     
     for(int i=1 ; i<=n ; i++) for(int j=1 ; j<=m ; j++) if(t[i][j] == 'X')
     {
+        int cnt = 0;
         t[i][j] = 'x';
         queue<pair<int, int>> bfs;
-        queue<int> cleaner;
         bfs.push({i, j});
         while(!bfs.empty())
         {
@@ -50,29 +45,16 @@ int main()
             for(int k=0 ; k<4 ; k++)
             {
                 int ii = ni+di[k], jj = nj+dj[k];
-                if(ii < 1 || jj < 1 || ii > n || jj > m || t[ii][jj] != ' ') continue;
-                if(!mark[comp[ii][jj]])
+                if(ii < 1 || jj < 1 || ii > n || jj > m) continue;
+                if(water[ii][jj]) cnt++;
+                else if(t[ii][jj] == 'X')
                 {
-                    mark[comp[ii][jj]] = true;
-                    cleaner.push(comp[ii][jj]);
+                    t[ii][jj] = 'x';
+                    bfs.push({ii, jj});
                 }
-                cnt[comp[ii][jj]]++;
-            }
-            for(int k=0 ; k<4 ; k++)
-            {
-                int ii = ni+di[k], jj = nj+dj[k];
-                if(ii < 1 || jj < 1 || ii > n || jj > m || t[ii][jj] != 'X') continue;
-                t[ii][jj] = 'x';
-                bfs.push({ii, jj});
             }
         }
-        while(!cleaner.empty())
-        {
-            ans = max(ans, cnt[cleaner.front()]);
-            cnt[cleaner.front()] = 0;
-            mark[cleaner.front()] = false;
-            cleaner.pop();
-        }
+        ans = max(ans, cnt);
     }
     printf("%d\n",ans);
     return 0;
